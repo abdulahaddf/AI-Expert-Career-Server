17446eec36a85a42b6cb4673e315a2a3b6b6ac74
abdulahaddf <abdulahad.df1@gmail.com>
2023-09-28 10:55:28 +0600

    user routes

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Data-Base start
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.0iomqcc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version--------
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+//Exporting all mongoDB collections
+const database = client.db("AI-Expert");
+    exports.usersCollection = database.collection("users");
+
+//All routes
+const userRoutes = require("./routes/users");
+
+//middlewares
+app.use(userRoutes);
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// Data-Base end
+
+app.get("/", (req, res) => {
+  res.send("AI Expert server is running");
+});
+
+app.listen(port, () => {
+  console.log(`AI Expert Server is running on port: ${port}`);
+});
