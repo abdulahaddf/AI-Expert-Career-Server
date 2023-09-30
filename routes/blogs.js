e4a7cbf4b16601e146e7fc9974dd5b5f228a700e
abdulahaddf <abdulahad.df1@gmail.com>
2023-09-30 13:24:08 +0600

    a blog will be liked one time api

--- routes/blogs.js
+++ routes/blogs.js
@@ -62,9 +62,9 @@ router.patch("/update-blog/:id", async (req, res) => {
 router.patch("/update-comment/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        console.log(id);
+        // console.log(id);
         const commentData = req.body;
-        console.log(commentData)
+        // console.log(commentData)
         // Use ObjectId to create a filter for finding the blog post by ID
         const filter = { _id: new ObjectId(id) };
         
@@ -75,7 +75,7 @@ router.patch("/update-comment/:id", async (req, res) => {
         const options = { new: true };
         
         const updatedBlog = await blogsCollection.findOneAndUpdate(filter, update, options);
-      console.log(updatedBlog);
+    //   console.log(updatedBlog);
         if (!updatedBlog) {
             return res.status(404).json({ error: 'Blog post not found' });
         }
@@ -89,31 +89,36 @@ router.patch("/update-comment/:id", async (req, res) => {
 // api for like
 router.patch("/like/:id", async (req, res) => {
     try {
-        const id = req.params.id;
-        console.log(id);
-        const data = req.body;
-        console.log(data)
-        // Use ObjectId to create a filter for finding the blog post by ID
-        const filter = { _id: new ObjectId(id) };
-        
-        // Use the $push operator to add a comment to the comments array
-        const update = { $push: { likes: data } };
-
-        // Set the options to return the updated document
-        const options = { new: true };
-        
-        const updatedBlog = await blogsCollection.findOneAndUpdate(filter, update, options);
-      console.log(updatedBlog);
-        if (!updatedBlog) {
-            return res.status(404).json({ error: 'Blog post not found' });
-        }
-        
-        res.json(updatedBlog);
+      const id = req.params.id;
+      const data = req.body;
+      const { email } = data;
+  
+      // Use ObjectId to create a filter for finding the blog post by ID
+      const filter = { _id: new ObjectId(id), 'likes.email': { $ne: email } };
+  
+      // Use the $push operator to add a like to the likes array
+      const update = { $push: { likes: data } };
+  
+      // Set the options to return the updated document
+      const options = { new: true };
+  
+      // Find and update the blog post, checking if the user's email is not already in the likes array
+      const updatedBlog = await blogsCollection.findOneAndUpdate(filter, update, options);
+  
+      if (!updatedBlog) {
+        return res.status(404).json({ error: 'Blog post not found' });
+      }
+  
+      res.json(updatedBlog);
     } catch (error) {
-        console.error("Error updating blog:", error);
-        res.status(500).json({ message: "An error occurred" });
+      console.error("Error updating blog:", error);
+      res.status(500).json({ message: "An error occurred" });
     }
-});
+  });
+
+
+
+  
 
 
 
