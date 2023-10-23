dae2c7df94647da43fe87484efa220f33ad8a5f6
abdulahaddf <abdulahad.df1@gmail.com>
2023-10-24 02:47:39 +0600

    sending mail api

--- routes/mail.js
+++ routes/mail.js
@@ -5,57 +5,72 @@ const { OAuth2Client } = require("google-auth-library");
 
 // Replace with your Gmail API credentials (use environment variables)
 const oauth2Client = new OAuth2Client({
-    clientId: process.env.client_id,
-    clientSecret: process.env.client_secret,
-    // redirectUri: "YOUR_REDIRECT_URI",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  // redirectUri: "YOUR_REDIRECT_URI",
 });
 
 // Generate an OAuth URL for user consent
 router.get("/auth-url", (req, res) => {
-    const authUrl = oauth2Client.generateAuthUrl({
-        access_type: "offline", // Enables a refresh token
-        scope: ["https://www.googleapis.com/auth/gmail.send"],
-    });
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline", // Enables a refresh token
+    scope: ["https://www.googleapis.com/auth/gmail.send"],
+  });
 
-    res.json({ authUrl });
+  res.json({ authUrl });
 });
 
 // Handle the OAuth callback and send the email
 router.post("/send-email", async (req, res) => {
-    const { from, to, subject, text } = req.body;
-
+    console.log(req.body);
+    const { toEmail1, toEmail2, subject, message } = req.body;
+  
     // Use your access token to send the email
-    const accessToken = req.body.accessToken; // Make sure you send the access token from the frontend
+    // const accessToken = req.body.accessToken; // Make sure you send the access token from the frontend
+  
+    // const transporter = nodemailer.createTransport({
+    //   service: "Gmail",
+    //   auth: {
+    //     type: "OAuth2",
+    //     user: "developer.aiec@gmail.com",
+    //     clientId: process.env.CLIENT_ID,
+    //     clientSecret: process.env.CLIENT_SECRET,
+    //     accessToken: accessToken,
+    //   },
+    // });
 
     const transporter = nodemailer.createTransport({
-        service: "Gmail",
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
-            type: "OAuth2",
-            user: "developer.aiec@gmail.com",
-            clientId: process.env.client_id,
-            clientSecret: process.env.client_secret,
-            accessToken: accessToken,
+          user: process.env.USER,
+          pass: process.env.PASS,
         },
-    });
+      });
 
-    // Define the email content
+
+    // Define the email content with an array of recipient email addresses
     const mailOptions = {
-        from: from,
-        to: to,
-        subject: subject,
-        text: text,
+      from: "developer.aiec@gmail.com", 
+      to: [toEmail1, toEmail2], 
+      subject: subject,
+      text: message,
     };
 
     // Send the email
     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error("Error sending email: ", error);
-            res.status(500).json({ error: "Failed to send the email." });
-        } else {
-            console.log("Email sent: " + info.response);
-            res.status(200).json({ message: "Email sent successfully!" });
-        }
+      if (error) {
+        console.error("Error sending email: ", error);
+        res.status(500).json({ error: "Failed to send the email." });
+      } else {
+        console.log("Email sent: " + info.response);
+        res.status(200).json({ message: "Email sent successfully!" });
+      }
     });
-});
+  });
+  
+
 
 module.exports = router;
